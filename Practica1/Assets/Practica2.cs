e855ad86d4a97adec634b7e1ad55268041dbcd34
Lparres2000 <>
2024-02-29 16:38:56 +0100

    Practica 2 Terminada

---
+++ Practica1/Assets/Practica2.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+public class Practica2 : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        // Referencia al UIDocument
+        UIDocument uiDoc = GetComponent<UIDocument>();
+
+        // Referencia a la raiz del UIDocument
+        VisualElement rootVE = uiDoc.rootVisualElement;
+
+        // Creamos una Query de objetos de tipo VisualElement a partir de la raiz
+        UQueryBuilder<VisualElement> builder = new(rootVE);
+
+        // Creamos una lista con todos los elementos del Query
+        //List<VisualElement> listVE = builder.ToList();
+        // Recorremos la lista
+        //listVE.ForEach(element => print(element.name));
+
+        // Creamos una lista de Labels que sean de la clase "gogoname". 
+        List<Label> listGogonames = rootVE.Query<Label>(className: "gogoname").ToList();
+
+        // Recorremos la lista de labels
+        //listGogonames.ForEach(element => print(element.name));
+
+        // Imprimimos por consola el número de gogos, y recorremos la lista para obtener sus nombres
+        Debug.Log("Numero de gogos: " + listGogonames.Count);
+        Debug.Log("Nombres de los gogos: ");
+        listGogonames.ForEach(element => print(element.text));
+
+        // Obtenemos el objecto de la clase "selector".
+        VisualElement selector = rootVE.Query(className: "selector");
+
+        // Cambiamos el orden de los elementos de selector.
+        selector.style.flexDirection = FlexDirection.RowReverse;
+
+        // Creamos una lista con los fondos de los personajes.
+        List<VisualElement> bgPj = rootVE.Query(className: "bgpj").ToList();
+
+        // Eliminamos la imagen asociada al fondo de cada personaje.
+        bgPj.ForEach(element => element.style.backgroundImage = null);
+
+
+    }
+}
