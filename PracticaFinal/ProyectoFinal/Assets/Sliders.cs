96e554997ce0f6fbf24231503e4c1a55704d565d
Denisa <denisaju@ucm.es>
2024-05-06 09:48:03 +0200

    Bueno, así es más óptimo, creo

--- PracticaFinal/ProyectoFinal/Assets/Sliders.cs
+++ PracticaFinal/ProyectoFinal/Assets/Sliders.cs
@@ -12,29 +12,7 @@ public class Sliders : MonoBehaviour
 
         UQueryBuilder<VisualElement> builder = new(root);
 
-        //List<Slider> sliderList = root.Query<Slider>().ToList();
-
-        //sliderList.ForEach(slider =>
-        //{
-        //    //Escondemos el agarre y la barrita del slider, que quedan feísimas
-        //    VisualElement dragger = root.Q<VisualElement>("unity-dragger");
-        //    dragger.style.display = DisplayStyle.None;
-
-        //    VisualElement tracker = root.Q<VisualElement>("unity-tracker");
-        //    tracker.style.display = DisplayStyle.None;
-        //});
-
-        List<Slider> sliderList = new List<Slider>();
-
-
-        Slider s0 = root.Q<Slider>("Percentage");
-        sliderList.Add(s0);
-        Slider s1 = root.Query<Slider>().AtIndex(0);
-        sliderList.Add(s1);
-        Slider s2 = root.Query<Slider>().AtIndex(1);
-        sliderList.Add(s2);
-        Slider s3 = root.Query<Slider>().AtIndex(2);
-        sliderList.Add(s3);
+        List<Slider> sliderList = root.Query<Slider>().ToList();
 
         sliderList.ForEach(slider =>
         {
@@ -45,6 +23,28 @@ public class Sliders : MonoBehaviour
             VisualElement tracker = slider.Q<VisualElement>("unity-tracker");
             tracker.style.display = DisplayStyle.None;
         });
+
+        //List<Slider> sliderList = new List<Slider>();
+
+
+        //Slider s0 = root.Q<Slider>("Percentage");
+        //sliderList.Add(s0);
+        //Slider s1 = root.Query<Slider>().AtIndex(0);
+        //sliderList.Add(s1);
+        //Slider s2 = root.Query<Slider>().AtIndex(1);
+        //sliderList.Add(s2);
+        //Slider s3 = root.Query<Slider>().AtIndex(2);
+        //sliderList.Add(s3);
+
+        //sliderList.ForEach(slider =>
+        //{
+        //    //Escondemos el agarre y la barrita del slider, que quedan feísimas
+        //    VisualElement dragger = slider.Q<VisualElement>("unity-dragger");
+        //    dragger.style.display = DisplayStyle.None;
+
+        //    VisualElement tracker = slider.Q<VisualElement>("unity-tracker");
+        //    tracker.style.display = DisplayStyle.None;
+        //});
 
       
 
